8388921bb7f4b8a1ee1f2014aa9a225a6ad0a91b
Jorvico <108956242+Jorvico@users.noreply.github.com>
2022-09-14 21:20:15 -0500

    Se crea el front para categorias y su funcionalidad

--- src/main/java/com/globalmarket/controlers/ControladorPrincipal.java
+++ src/main/java/com/globalmarket/controlers/ControladorPrincipal.java
@@ -1,10 +1,14 @@
 package com.globalmarket.controlers;
 
+import com.globalmarket.entities.Categoria;
 import com.globalmarket.services.CategoriaServicio;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 /**
@@ -39,14 +43,36 @@ public class ControladorPrincipal {
         }else{
             model.addAttribute("categorias", servicio.consultarCategoria(criterio));
         }
-
         return "listacategorias";
+    }
 
+    @PostMapping("/categorias")
+    public String guardarCategoria(@ModelAttribute("categoria") Categoria c){
+        servicio.crearNuevaCategoria(c);
+        return "redirect:/categorias";
     }
 
     @GetMapping("/categorias/form")
-    public String cargarFormCategorias() {
+    public String cargarFormNuevaCategorias(Model modelo){
+        Categoria c = new Categoria();
+        modelo.addAttribute("categoria", c);
         return "formcategorias";
-
     }
+    
+    @GetMapping("/categorias/form/{id}")
+    public String cargarFormCategoriasExistente(Model modelo, @PathVariable int id){
+        Categoria c = servicio.consultarCategoria(id);
+        modelo.addAttribute("categoria", c);
+        return "formcategorias";
+    }
+    
+    
+    @GetMapping("/categorias/eliminar/{id}")
+    public String eliminarCategoriaExistente(Model modelo, @PathVariable int id){
+        Categoria c = servicio.consultarCategoria(id);
+        servicio.eliminarCategoria(c);
+        return "redirect:/categorias";
+    }
+    
+    
 }
